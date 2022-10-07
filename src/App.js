811b634f85c16732fe57e7ac6850c79db05a0aa0
matheus <mathdiogenes@gmail.com>
2022-10-07 18:39:33 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Settings from './components/Settings';
 
 export default function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/settings" component={ Settings } />
       </Switch>
 
     </div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Login extends Component {
   state = {
@@ -53,6 +54,14 @@ export default class Login extends Component {
         >
           Play
         </button>
+        <Link to="/settings">
+          <button
+            type="button"
+            data-testid="btn-settings"
+          >
+            Settings
+          </button>
+        </Link>
       </form>
     );
   }
