195bac856df4a90bfa76d7d9080ce53019890745
Thalis Venancio <devthalis@gmail.com>
2022-10-07 20:11:46 -0300

    Segundo requisito concluido sem erros de lint

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,20 +1,7 @@
 import React from 'react';
 
 class Home extends React.Component {
-  state = {
-    token: '',
-  };
-
-  componentDidMount() {
-    const getToken = localStorage.getItem('token');
-    this.setState({
-      token: getToken,
-    });
-  }
-
   render() {
-    const { token } = this.state;
-    console.log(token);
     return (
       <div>Pagina incial do jogo</div>
     );
