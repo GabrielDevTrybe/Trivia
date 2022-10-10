f9cf3ad469567e57f21b138e3637a06cc06b2f19
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-10-10 16:40:42 -0300

    requisito 16 e 18 feito

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
+import Ranking from './pages/Ranking';
 import Game from './pages/Game';
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <Route exact path="/" component={ Login } />
         <Route path="/settings" component={ Settings } />
         <Route path="/game" component={ Game } />
+        <Route path="/ranking" component={ Ranking } />
       </Switch>
 
     </div>
